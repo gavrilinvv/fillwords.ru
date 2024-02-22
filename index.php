10ca8a17ea77fdec64e112fb1730678208f02c88
Slava <gavrilin2410@gmail.com>
2024-02-22 17:23:07 +0300

    fixes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 	<meta property="og:title" content="Филворды | Браузерная игра"/>
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://alchemy-game.ru" />
+	<meta property="og:url" content="https://fillwords.ru" />
 	<meta name="description" content="Находите слова в простой головоломке для всей семьи. Онлайн, бесплатно и без регистрации" />
 	<meta property="og:description" content="Находите слова в простой головоломке для всей семьи. Онлайн, бесплатно и без регистрации" />
 	<meta name="keywords" content="Филворды, бесплатная браузерная игра, головоломка, играть без регистрации, онлайн, найти слова, на поле" />
