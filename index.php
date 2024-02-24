7f4ed059acbd63522ea6452a46b77b24beae2ac8
Slava <gavrilin2410@gmail.com>
2024-02-24 21:30:25 +0300

    add yandex metrika

--- index.php
+++ index.php
@@ -12,6 +12,24 @@
 	<meta property="og:description" content="Находите слова в простой головоломке для всей семьи. Онлайн, бесплатно и без регистрации" />
 	<meta name="keywords" content="Филворды, бесплатная браузерная игра, головоломка, играть без регистрации, онлайн, найти слова, на поле" />
 	<meta name="yandex-verification" content="738fcd3e75cf5f96" />
+
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+		(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+		m[i].l=1*new Date();
+		for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+		k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+		(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+		ym(96564571, "init", {
+				clickmap:true,
+				trackLinks:true,
+				accurateTrackBounce:true,
+				webvisor:true
+		});
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/96564571" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
 </head>
 <body>
 	<div id="app">
